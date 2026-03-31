8f4179d16ba47d99f5353f1191fe4cd27dbd3f12
Martin <mmartinr25@ilb.cat>
2026-03-31 10:05:33 +0200

    elements del DOM per relacionar script amb html

---
+++ script.js
@@ -0,0 +1,14 @@
+// elements del DOM per relacionar script amb html
+let formulari = document.getElementById("formulariAlumne");
+let inputNom = document.getElementById("nom");
+let inputExamen = document.getElementById("examen");
+let inputPractiques = document.getElementById("practiques");
+let inputActitud = document.getElementById("actitud");
+let missatge = document.getElementById("missatge");
+let cosTaula = document.getElementById("cosTaula");
+
+let botoAsc = document.getElementById("ordenarAsc");
+let botoDesc = document.getElementById("ordenarDesc");
+
+// Array per els alumnes
+let alumnes = [];
